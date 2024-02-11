87eab75b9ece62069141f256bf63ade18ee80b98
Tom Schindl <tom.schindl@bestsolution.at>
2024-02-11 10:31:00 +0100

    avoid exception

--- server/src/main/java/at/bestsolution/qutime/handler/event/GetHandler.java
+++ server/src/main/java/at/bestsolution/qutime/handler/event/GetHandler.java
@@ -3,14 +3,11 @@ package at.bestsolution.qutime.handler.event;
 import java.time.ZoneId;
 import java.util.UUID;
 
-import at.bestsolution.qutime.Utils;
 import at.bestsolution.qutime.dto.EventDTO;
 import at.bestsolution.qutime.model.EventEntity;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.NoResultException;
-import jakarta.ws.rs.core.Response;
 
 @Singleton
 public class GetHandler {
@@ -22,14 +19,16 @@ public class GetHandler {
     }
 
     public EventDTO get(UUID calendarKey, UUID eventKey, ZoneId zone) {
-        try {
-            var query = em.createQuery("FROM Event e WHERE e.key = :eventKey AND e.calendar.key = :calendarKey", EventEntity.class);
-            query.setParameter("eventKey", eventKey);
-            query.setParameter("calendarKey", calendarKey);
-            var result = query.getSingleResult();
-            return EventDTO.of(result, zone);
-        } catch( NoResultException e ) {
+        
+        var query = em.createQuery("FROM Event e WHERE e.key = :eventKey AND e.calendar.key = :calendarKey", EventEntity.class);
+        query.setParameter("eventKey", eventKey);
+        query.setParameter("calendarKey", calendarKey);
+        var result = query.getResultList();
+        if( result.size() == 1 ) {
+            return EventDTO.of(result.get(0), zone);
+        } else if( result.size() == 0 ) {
             return null;
         }
+        throw new IllegalStateException(String.format("Multiple matching events for '%s' are found.", eventKey));
     }
 }
