4fb73d493ad318f62779605b84a5f83afef05c12
Sebastian Sams <sebastian.sams@bestsolution.at>
2024-02-23 10:19:29 +0100

    ensure DB setup script fails on errors

--- server/src/database/setup.sh
+++ server/src/database/setup.sh
@@ -6,7 +6,7 @@ DATA_DIR=${SETUP_SQL_DATA_DIR:-./sql}
 versionexists=$(psql -qtAc "SELECT to_regclass('meta_dbversion')")
 if [ -z "$versionexists" ]; then
   echo "setting up database structure..."
-  psql --single-transaction -f ${DATA_DIR}/setup_dbversion.sql -f ${DATA_DIR}/structure.sql
+  psql -v ON_ERROR_STOP=1 --single-transaction -f ${DATA_DIR}/setup_dbversion.sql -f ${DATA_DIR}/structure.sql
 fi
 
 # run all migrations if needed (in deployments the complete script is run against an existing
@@ -16,5 +16,5 @@ fi
 #dbversion=$(psql -qtAc "SELECT version FROM meta_dbversion ORDER BY id DESC LIMIT 1")
 #if [ "$dbversion" -lt 1 ]; then
 #  echo "running DB upgrades for version 0.2.0..."
-#  psql --single-transaction -f ${DATA_DIR}/migrations/upgrade-0.2.0.sql
+#  psql -v ON_ERROR_STOP=1 --single-transaction -f ${DATA_DIR}/migrations/upgrade-0.2.0.sql
 #fi
