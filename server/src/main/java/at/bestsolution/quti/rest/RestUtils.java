8b5b379fc486da551c815f082d8ef068ceb4f5ae
Tom Schindl <tom.schindl@bestsolution.at>
2025-02-15 16:48:41 +0100

    send error type as part of the response

--- server/src/main/java/at/bestsolution/quti/rest/RestUtils.java
+++ server/src/main/java/at/bestsolution/quti/rest/RestUtils.java
@@ -8,10 +8,12 @@ public class RestUtils {
 
 	public static Response toResponse(int status, _RSDException e) {
 		if (e instanceof _RSDException.RSDMessageException m) {
-			return Response.status(status).entity(m.message).build();
+			return Response.status(status).header("X-RSD-Error-Type", e.type).entity(_JsonUtils.encodeAsJsonString(m.message))
+					.build();
 		} else if (e instanceof _RSDException.RSDStructuredDataException s) {
-			return Response.status(status).entity(_JsonUtils.toJsonString(s.data, false)).build();
+			return Response.status(status).header("X-RSD-Error-Type", e.type).entity(_JsonUtils.toJsonString(s.data, false))
+					.build();
 		}
-		return Response.serverError().build();
+		return Response.serverError().entity("Unknown error '%'".formatted(e.getClass().getName())).build();
 	}
 }
