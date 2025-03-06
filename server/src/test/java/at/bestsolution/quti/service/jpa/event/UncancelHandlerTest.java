8d14e5da532016be47057116bd2d02c3b450c5d3
Tom Schindl <tom.schindl@bestsolution.at>
2025-03-06 09:57:18 +0100

    remove stale imports

--- server/src/test/java/at/bestsolution/quti/service/jpa/event/UncancelHandlerTest.java
+++ server/src/test/java/at/bestsolution/quti/service/jpa/event/UncancelHandlerTest.java
@@ -1,9 +1,7 @@
 package at.bestsolution.quti.service.jpa.event;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
