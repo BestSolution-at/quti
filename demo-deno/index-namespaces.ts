d23568abc7fc49be5e749272999a43d7ae1332b3
Tom Schindl <tom.schindl@bestsolution.at>
2025-03-15 13:21:24 +0100

    wip ts demo

---
+++ demo-deno/index-namespaces.ts
@@ -0,0 +1,3 @@
+export * as model from './model/index.ts';
+export * as service from './index-service.ts';
+export * as utils from './_type-utils.ts';
---
+++ demo-deno/index.ts
@@ -0,0 +1,1 @@
+export * as api from './index-namespaces.ts';
